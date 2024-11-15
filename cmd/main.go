4b28ce73c2355917a82903c8fbb2e9a48129c5c5
MiguelBad <badayosbiboy@gmail.com>
2024-11-15 22:56:35 +0930

    for now...

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(req *http.Request) bool { return true },
+}
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Player)
+
+type Player struct {
+	conn *websocket.Conn
+	id   string
+	pos  Position
+	mu   sync.Mutex
+}
+
+type Position struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+
+	log.Fatal(http.ListenAndServe(":1205", nil))
+}
+
+func handleConnections(w http.ResponseWriter, req *http.Request) {
+	conn, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
+
+	clients[conn] = true
+	for {
+	}
+}
