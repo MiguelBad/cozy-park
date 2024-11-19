dad63628c50862add830722695f633fa236a2770
MiguelBad <badayosbiboy@gmail.com>
2024-11-20 00:56:32 +0930

    fixed movement

--- web/src/index.js
+++ web/src/index.js
@@ -1,39 +1,15 @@
 /**
  * @typedef {{ blueWalkLeft: HTMLImageElement[]; blueWalkRight: HTMLImageElement[]; pinkWalkLeft: HTMLImageElement[]; pinkWalkRight: HTMLImageElement[]; }} PlayerFrame
  * @typedef {{ action: string, target: string, x: number, y: number, frame: number, direction: string}} Status
- * @typedef {{ id: string, status: Status}} PlayerState
+ * @typedef { Object<string, Status>} PlayerState
  */
+
+var userId = "";
 
 document.addEventListener("DOMContentLoaded", async function () {
     const playerFrame = await fetchPlayerFrames();
     main(playerFrame);
 });
-
-class handleMovement {
-    /**
-     * @param {number} x
-     * @param {number} y
-     */
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.moving = false;
-    }
-
-    /**
-     * @param {number} end
-     * @param {number} alpha
-     * @returns {{x: number, y: number}}
-     */
-    interpolate(end, alpha) {
-        return {
-            x: this.x + (this.x - end) * alpha,
-            y: this.y + (this.y - end) * alpha,
-        };
-    }
-
-    extrapolate() {}
-}
 
 /**
  * @param {PlayerFrame} playerFrame
@@ -57,6 +33,60 @@ function main(playerFrame) {
 
     const socket = new WebSocket("ws://localhost:1205/ws");
 
+    /**
+     * @type {PlayerState}
+     */
+    const playerState = {};
+    socket.onmessage = (event) => {
+        const serverMessage = JSON.parse(event.data);
+        switch (serverMessage.type) {
+            case "playerState":
+                playerState[serverMessage.id] = serverMessage.status;
+                break;
+            case "clientInfo":
+                userId = serverMessage.id;
+                break;
+        }
+    };
+
+    /**
+     * @type {string}
+     */
+    let lastPressed;
+    window.addEventListener("keydown", (event) => {
+        if (lastPressed === event.key) {
+            return;
+        }
+
+        console.log(userId);
+        lastPressed = event.key;
+
+        const pos = {
+            x: playerState[userId].x,
+            y: playerState[userId].y,
+        };
+        switch (event.key) {
+            case "w":
+                pos.y += 10;
+                break;
+            case "a":
+                pos.x -= 10;
+                break;
+            case "s":
+                pos.y += 10;
+                break;
+            case "d":
+                pos.x += 10;
+                break;
+        }
+
+        socket.send(JSON.stringify(pos));
+    });
+
+    window.addEventListener("keyup", () => {
+        lastPressed = "";
+    });
+
     const fps = 12;
     const interval = 1000 / fps;
     let lastFrameTime = 0;
@@ -67,44 +97,8 @@ function main(playerFrame) {
         const elapsed = timestamp - lastFrameTime;
         if (elapsed > interval) {
             lastFrameTime = timestamp - (elapsed % interval);
-
-            /**
-             * @type{PlayerState}
-             */
-            let playerState;
-
-            socket.onmessage = (event) => {
-                playerState = JSON.parse(event.data);
-                renderGame(playerState, canvasCtx, playerFrame);
-            };
-
-            /**
-             * @type {string}
-             */
-            let lastPressed;
-            window.addEventListener("keydown", (event) => {
-                if (lastPressed === event.key) {
-                    return;
-                }
-
-                lastPressed = event.key;
-                switch (event.key) {
-                    case "w":
-                        break;
-                    case "a":
-                        break;
-                    case "s":
-                        break;
-                    case "d":
-                        break;
-                }
-            });
-
-            window.addEventListener("keyup", () => {
-                lastPressed = "";
-            });
+            renderGame(playerState, canvasCtx, playerFrame);
         }
-
         requestAnimationFrame(gameLoop);
     }
 
@@ -118,5 +112,34 @@ function main(playerFrame) {
  */
 function renderGame(playerState, context, playerFrame) {
     context.clearRect(0, 0, 1000, 500);
-    context.drawImage(playerFrame.blueWalkRight[0], playerState.status.x, playerState.status.y);
+    for (const player in playerState) {
+        const state = playerState[player];
+        context.drawImage(playerFrame.blueWalkRight[0], state.x, state.y);
+    }
 }
+
+// class handleMovement {
+//     /**
+//      * @param {number} x
+//      * @param {number} y
+//      */
+//     constructor(x, y) {
+//         this.x = x;
+//         this.y = y;
+//         this.moving = false;
+//     }
+//
+//     /**
+//      * @param {number} end
+//      * @param {number} alpha
+//      * @returns {{x: number, y: number}}
+//      */
+//     interpolate(end, alpha) {
+//         return {
+//             x: this.x + (this.x - end) * alpha,
+//             y: this.y + (this.y - end) * alpha,
+//         };
+//     }
+//
+//     extrapolate() {}
+// }
